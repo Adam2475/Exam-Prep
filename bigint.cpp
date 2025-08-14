18dcb4a52d54881e9282346e543e9070ed70a982
Adam <103849622+Adam2475@users.noreply.github.com>
2025-08-14 09:30:04 +0000

    operator + logic solved

--- bigint.cpp
+++ bigint.cpp
@@ -22,12 +22,59 @@ bigint &bigint::operator=(const bigint &ptref)
 bigint bigint::operator+(const bigint &addend) const
 {
     bigint result;
-    bigint op1 = *this;
-    bigint op2 = addend;
-    reverse(op1.number.begin(), op1.number.end());
-    reverse(op2.number.begin(), op2.number.end());
-    cout << op1;
-    cout << op2; 
+    string tmpResult;
+    string op1 = this->number;
+    string op2 = addend.number;
+    reverse(op1.begin(), op1.end());
+    reverse(op2.begin(), op2.end());
+    // cout << "added " << op1;
+    // cout << "addend " << op2;
+    
+    int tmp;
+    int i = 0;
+
+    char *string1 = (char *)op1.c_str();
+    char *string2 = (char *)op2.c_str(); 
+
+    char tmpChar1;
+    char tmpChar2;
+
+    int rest = 0;
+
+    int res;
+
+    int i1;
+    int i2;
+
+    int tmpInt1;
+    int tmpInt2;
+
+    while (op2[i] != '\0' && op1[i] != '\0')
+    {
+        tmpChar1 = (char)string1[i];
+        tmpChar2 = (char)string2[i];
+        // cout << "arg1 : " << tmpChar1 << "arg2 : " << tmpChar2 << endl;
+        // tmpInt1 = atoi(&tmpChar1);
+        // tmpInt2 = atoi(&tmpChar2);
+        tmpInt1 = tmpChar1 - '0';
+        tmpInt2 = tmpChar2 - '0';
+        tmp = tmpInt1 + tmpInt2;
+        //cout << "arg1 : " << tmpInt1 << "arg2 : " << tmpInt2 << endl;
+
+        if (tmp < 10)
+        {
+            i1 = tmp / 10;
+            i2 = tmp % 10;
+            rest = 1;
+            tmp = i2;
+        }
+        tmpResult += tmp + '0';
+        // cout << "for i = " << i << ": " << atoi(&string1[i]);
+        i++;
+    }
+    reverse(tmpResult.begin(), tmpResult.end());
+    // cout << "result" << tmpResult << endl;
+    result.number = tmpResult;
     return result;
 }
 
